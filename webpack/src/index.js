86d5a9341b2296222cf040d674f0ec08560fda8d
aamir1995 <syedaamir974@gmail.com>
2022-04-25 12:35:02 +0500

    Updated webgl-api package, changed the default scene to Cube

--- webpack/src/index.js
+++ webpack/src/index.js
@@ -7,14 +7,14 @@ async function main(canvas) {
     // Create a view
     const view = await api.createView({ background: { color: [0, 0, 0.25, 1] } }, canvas);
 
-    // load a predfined scene into the view, available views are cube, oilrig, condos
-    view.scene = await api.loadScene(NovoRender.WellKnownSceneUrls.condos);
+    // load a predefined scene into the view, available views are cube, oilrig, condos
+    view.scene = await api.loadScene(NovoRender.WellKnownSceneUrls.cube);
 
     // provide a controller, available controller types are static, orbit, flight and turntable
     view.camera.controller = api.createCameraController({ kind: "turntable" }, canvas);
 
     const ctx = canvas.getContext("bitmaprenderer");
-    for (; ;) {
+    for (; ;) { // render-loop https://dens.website/tutorials/webgl/render-loop
 
         const { clientWidth: width, clientHeight: height } = canvas;
         // handle resizes
