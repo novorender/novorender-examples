2d6e51f9dabc811e94ee0352882e319239a479ea
Aamir Shah <syedaamir974@gmail.com>
2023-09-26 22:53:13 +0500

    feat: vite-react-ts

---
+++ vite-react-ts/README.md
@@ -0,0 +1,29 @@
+# Using @novorender/api with Vite [react-ts](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) template
+
+1. Clone the repository:
+
+    ```bash
+    git clone https://github.com/novorender/novorender-examples.git
+    ```
+
+2. Navigate into the project directory:
+
+    ```bash
+        cd vite-react-ts
+    ```
+
+3. Install project dependencies:
+
+    ```bash
+    npm i
+    ```
+
+4. Run the development server:
+
+    ```bash
+    npm run dev
+    ```
+
+**--- OR ---**
+
+[![Open in StackBlitz](https://developer.stackblitz.com/img/open_in_stackblitz.svg)](https://stackblitz.com/github/novorender/novorender-examples/tree/master/vite-react-ts?file=src/App.tsx)
---
+++ vite-react-ts/src/App.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from 'react'
+import { View, getDeviceProfile, createSphereObject } from "@novorender/api";
+import './App.css'
+
+const { mesh } = createSphereObject();
+
+function App() {
+
+  const canvas = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    main(canvas.current);
+  }, [])
+
+  // Create a simple sphere mesh object.
+
+  async function main(canvas: HTMLCanvasElement | null) {
+    const gpuTier = 2; // laptop with reasonably new/powerful GPU.
+    // Get Device Profile
+    const deviceProfile = getDeviceProfile(gpuTier);
+    const imports = await View.downloadImports({ baseUrl: "/novorender/api/" }); // or whereever you copied the public/ files from the package.
+    // Create a View
+    const view = new View(canvas as HTMLCanvasElement, deviceProfile, imports);
+    // load a predefined environment to set it as background
+    const envIndexUrl = "https://api.novorender.com/assets/env/index.json";
+    const envs = await view.availableEnvironments(envIndexUrl);
+    const { url } = envs[2]; // just pick one
+    // modify the render state
+    view.modifyRenderState({
+      background: { url, blur: 0 }, // may take a while to download
+      grid: { enabled: true },
+      dynamic: {
+        objects: [{
+          mesh, // add a metallic sphere
+          instances: [{ position: [0, 0, 0], scale: 3 }]
+        }]
+      }
+    });
+    // run the view
+    await view.run();
+    // dispose-off GPU resources
+    view.dispose();
+  }
+
+  return (
+    <>
+      <canvas ref={canvas} style={{ width: '100%', height: '100%' }}></canvas>
+    </>
+  )
+}
+
+export default App
