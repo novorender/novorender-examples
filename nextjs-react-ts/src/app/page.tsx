b99589475b79e666b375fbbf1635f88b7b38b2a4
Aamir Shah <syedaamir974@gmail.com>
2024-12-09 15:04:53 +0500

    fix(next-react-ts): build fix

--- nextjs-react-ts/src/app/page.tsx
+++ nextjs-react-ts/src/app/page.tsx
@@ -1,57 +1,20 @@
 "use client";
 
-import { useEffect, useRef } from "react";
-import { createSphereObject, getDeviceProfile, View } from "@novorender/api";
+import dynamic from "next/dynamic";
 
-const { mesh } = createSphereObject();
+/**
+ * Novorender relies on browser-only APIs like window,
+ * we need disable server-side rendering for our component.
+ */
+const Sphere = dynamic(
+  () => import('./sphere'),
+  { ssr: false }
+)
 
 export default function Home() {
-  const canvas = useRef<HTMLCanvasElement>(null);
-
-  useEffect(() => {
-    main(canvas.current);
-  }, []);
-
-  // Create a simple sphere mesh object.
-
-  async function main(canvas: HTMLCanvasElement | null) {
-    const gpuTier = 2; // laptop with reasonably new/powerful GPU.
-    // Get Device Profile
-    const deviceProfile = getDeviceProfile(gpuTier);
-    const baseUrl = new URL("/novorender/api/", location.origin);
-    const imports = await View.downloadImports({ baseUrl }); // or wherever you copied the public/ files from the package.
-    // Create a View
-    const view = new View(canvas as HTMLCanvasElement, deviceProfile, imports);
-    // load a predefined environment to set it as background
-    const envIndexUrl = new URL(
-      "https://api.novorender.com/assets/env/index.json",
-    );
-    const envs = await View.availableEnvironments(envIndexUrl);
-    const { url } = envs[2]; // just pick one
-    // modify the render state
-    view.modifyRenderState({
-      background: { url, blur: 0 }, // may take a while to download
-      grid: { enabled: true },
-      dynamic: {
-        objects: [{
-          mesh, // add a metallic sphere
-          instances: [{ position: [0, 0, 0], scale: 3 }],
-        }],
-      },
-    });
-    // run the view
-    await view.run();
-    // dispose-off GPU resources
-    view.dispose();
-  }
-
   return (
     <main>
-      <canvas
-        ref={canvas}
-        style={{ width: "100%", height: "100%", background: "grey" }}
-      >
-      </canvas>
+      <Sphere />
     </main>
   );
 }
---
+++ nextjs-react-ts/src/app/sphere.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useRef } from "react";
+import { createSphereObject, getDeviceProfile, View } from "@novorender/api";
+
+const { mesh } = createSphereObject();
+
+export default function Sphere() {
+    const canvas = useRef<HTMLCanvasElement>(null);
+
+    useEffect(() => {
+        main(canvas.current);
+    }, []);
+
+    // Create a simple sphere mesh object.
+    async function main(canvas: HTMLCanvasElement | null) {
+        const gpuTier = 2; // laptop with reasonably new/powerful GPU.
+        // Get Device Profile
+        const deviceProfile = getDeviceProfile(gpuTier);
+        const baseUrl = new URL("/novorender/api/", location.origin);
+        const imports = await View.downloadImports({ baseUrl }); // or wherever you copied the public/ files from the package.
+        // Create a View
+        const view = new View(canvas as HTMLCanvasElement, deviceProfile, imports);
+        // load a predefined environment to set it as background
+        const envIndexUrl = new URL(
+            "https://api.novorender.com/assets/env/index.json",
+        );
+        const envs = await View.availableEnvironments(envIndexUrl);
+        const { url } = envs[2]; // just pick one
+        // modify the render state
+        view.modifyRenderState({
+            background: { url, blur: 0 }, // may take a while to download
+            grid: { enabled: true },
+            dynamic: {
+                objects: [{
+                    mesh, // add a metallic sphere
+                    instances: [{ position: [0, 0, 0], scale: 3 }],
+                }],
+            },
+        });
+        // run the view
+        await view.run();
+        // dispose-off GPU resources
+        view.dispose();
+    }
+
+    return (
+        <canvas
+            ref={canvas}
+            style={{ width: "100%", height: "100%", background: "grey" }}
+        >
+        </canvas>
+    );
+}
